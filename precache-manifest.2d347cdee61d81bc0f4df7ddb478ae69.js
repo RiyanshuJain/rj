119053b1f280c124cdded5de7abe98e34e1653fa
unknown <jain.59@iitj.ac.in>
2022-12-23 19:19:06 +0530

    Updates

---
+++ precache-manifest.2d347cdee61d81bc0f4df7ddb478ae69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0f26cc22daf4f0e77699fd9f065f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95d7bf6a3a647228c56c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "50581763d05dbae5e601",
+    "url": "/static/css/main.03c5ffc4.chunk.css"
+  },
+  {
+    "revision": "95d7bf6a3a647228c56c",
+    "url": "/static/js/2.704e10d8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.704e10d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50581763d05dbae5e601",
+    "url": "/static/js/main.54c55011.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
